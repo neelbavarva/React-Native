366b8d7aac9e2e90d0bf7a55001b31eb461d559c
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-21 13:10:04 +0530

    welcome screen added

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,32 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import AppButton from './app/components/AppButton';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-
-  const handlePress = () => console.log("PRESSED");
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
-      <Image 
-      blurRadius={10}
-      fadeDuration={1000}
-      source={{
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300"}}/>
-        {/* Random comment */}
-      <StatusBar style="auto" />
-    </SafeAreaView>
+      <ViewImageScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
---
+++ DoneWithIt/app/screens/WelcomeScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground 
+            style={styles.background}
+            source={require('../assets/background.jpg')}>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+                </View>
+                <View style={styles.loginButton}></View>
+                <View style={styles.registerButton}></View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 70,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: "center",
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#fc5c65",
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#4ecdc4",
+    }
+    
+})
+
+export default WelcomeScreen;
